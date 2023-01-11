041182ad70ea62d4b33175e5fc4f735fe0fd100a
mostalt <mostalt.mail@gmail.com>
2023-01-11 21:22:09 +0700

    feat(): simple api server

--- backend/main.go
+++ backend/main.go
@@ -1,18 +1,51 @@
 package main
 
 import (
+  "fmt"
   "net/http"
 
+  "github.com/gin-contrib/cors"
   "github.com/gin-gonic/gin"
 )
 
 func main() {
-  router := gin.Default()
-  router.GET("/api", getCustomResp)
+  router := gin.New()
 
+  // Set a lower memory limit for multipart forms (default is 32 MiB)
+	router.MaxMultipartMemory = 8 << 20  
+
+  router.Use(gin.Logger())
+
+  // todo auth
+  config := cors.DefaultConfig()
+  config.AllowOrigins = []string{"http://localhost:3000"}
+  router.Use(cors.New(config))
+
+  // Recovery middleware recovers from any panics and writes a 500 if there was one.
+  router.Use(gin.Recovery())
+
+  router.POST("/api/upload", handleFileUpload)
+
+  router.GET("/", func(context *gin.Context) {
+    context.JSON(http.StatusOK, "")
+  })
+
+  // todo: move to envs
   router.Run("localhost:8080")
 }
 
-func getCustomResp(c *gin.Context) {
-  c.IndentedJSON(http.StatusOK, "all good, bra")
+func handleFileUpload(c *gin.Context) {
+  form, err := c.MultipartForm()
+
+  if err != nil {
+    return
+  }
+
+  files := form.File["files"]
+
+  for _, file := range files {
+    fmt.Println(file.Filename)
+  }
+
+  c.String(http.StatusOK, fmt.Sprintf("%d files uploaded!", len(files)))
 }
