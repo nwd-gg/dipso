9fa4b1652e2c64520a8a2781a95d1cf4c62b3050
mostalt <mostalt.mail@gmail.com>
2023-02-15 20:23:44 +0700

    wip

---
+++ backend/utils/image_handler/image_handler.go
@@ -0,0 +1,14 @@
+package image_handler
+
+/*
+#include <magick/MagickCore.h>
+#include <wand/MagickWand.h>
+*/
+import (
+	"C"
+	"fmt"
+)
+
+func RunCommand() {
+	fmt.Print("TEST!!!!!!!!!!!!!!!!!!!!!")
+}
